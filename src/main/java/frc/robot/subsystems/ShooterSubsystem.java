cd67af5ab897bec9742eecda8063117f41eab586
Lyric Santana <LyricKSantana@gmail.com>
2026-03-03 15:39:28 -0800

    Put shooter controls in periodic

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -45,18 +45,13 @@ public class ShooterSubsystem extends SubsystemBase {
     double shooterWheelSpeed = 0;
     double intakeWheelSpeed = 0;
     double defaultShooterSpeed = 20;
-    double defaultIntakeSpeed = -8;
 
     final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
     
     public Command runShooter(){
         return run(()-> {
             shooterWheelSpeed = defaultShooterSpeed;
-            intakeWheelSpeed = defaultIntakeSpeed;
-            intakeMotor.setControl(new VoltageOut(intakeWheelSpeed));
-            shooterMotorRight.setControl(m_request.withVelocity(shooterWheelSpeed));
-            shooterWheelSpeed = 0;
-            intakeWheelSpeed = 0;
+            intakeWheelSpeed = 8;
         });
     }
 
@@ -66,5 +61,9 @@ public class ShooterSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
+        intakeMotor.setControl(new VoltageOut(intakeWheelSpeed));
+        shooterMotorRight.setControl(m_request.withVelocity(-1 * shooterWheelSpeed));
+        shooterWheelSpeed = 0;
+        intakeWheelSpeed = 0;
     }    
 }  
